0e6f1d6e18e6e68e1249c0c0c3502da69650c569
nhut2000dn <47530979+nhut2000dn@users.noreply.github.com>
2021-01-09 15:59:48 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from predict import response
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self, text):
+        return response(text)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to CodingX</h1>"
+
+        
+
+api.add_resource(Employees, '/send/<text>') 
+
+if __name__ == '__main__':
+     app.run()
